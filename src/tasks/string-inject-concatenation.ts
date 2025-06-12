ef381c8422b89bc3a95681082349a2d1209270e6
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-12 15:57:25 +0200

    feat: Renamed variable "result" to "resultInjectStrings".

--- src/tasks/string-inject-concatenation.ts
+++ src/tasks/string-inject-concatenation.ts
@@ -1,6 +1,6 @@
 const fixedMiddleText: string = "Love";
 
 const injectStrings = (userInputPrefixText: string, userInputSuffixText: string): void => {
-    const result = [userInputPrefixText, fixedMiddleText, userInputSuffixText].join(" ");
-    console.log(result);
+    const resultInjectStrings = [userInputPrefixText, fixedMiddleText, userInputSuffixText].join(" ");
+    console.log(resultInjectStrings);
 };
