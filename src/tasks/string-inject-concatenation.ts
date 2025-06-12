8af225160e530ed3711a3c55df317e5a3d3119ff
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-12 15:28:37 +0200

    feat: Task 2: String inject concatenation. Added function to concatenate two strings with fixed middle text.

---
+++ src/tasks/string-inject-concatenation.ts
@@ -0,0 +1,5 @@
+const userInputPrefixText: string = "Sweetie";
+const userInputSuffixText: string = "Darling";
+const fixedMiddleText: string = "Love";
+
+const concatenatedString: string = [userInputPrefixText, fixedMiddleText, userInputSuffixText].join(" ");
