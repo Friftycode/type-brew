c122f42b9cbddd4009ad28422166c0b3cab48cad
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-13 10:35:16 +0200

    feat: Moved variable fixedMiddleText within the function.

--- src/tasks/string-inject-concatenation.ts
+++ src/tasks/string-inject-concatenation.ts
@@ -1,9 +1,8 @@
-const fixedMiddleText: string = "love";
-
 const injectStrings = (
   userInputPrefixText: string,
   userInputSuffixText: string,
 ): void => {
+  const fixedMiddleText: string = "love";
   const resultInjectStrings = [
     userInputSuffixText,
     fixedMiddleText,
