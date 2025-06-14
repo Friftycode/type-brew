8febff9fe36f85695a053fe13a4428a3f2b9b830
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-14 21:09:02 +0200

    feat: Updated the function to return value instead of standard console.log it.

--- src/tasks/love-reminders.ts
+++ src/tasks/love-reminders.ts
@@ -1,15 +1,12 @@
-const printLoveReminders = (value: string | number | boolean): void => {
+const printLoveReminders = (value: string | number | boolean): string => {
   if (typeof value === "string") {
-    console.log(`I got you, bebbehh!`);
+    return `I got you, bebbehh!`;
   } else if (typeof value === "number") {
-    console.log(
-      `I’d travel to the moon and back ${value} times, just to be by your side.`,
-    );
+    return `I’d travel to the moon and back ${value} times, just to be by your side.`;
   } else if (typeof value === "boolean") {
-    console.log(
-      value
-        ? "I’ll always love you."
-        : "Even through the toughest times, I’ll still love you.",
-    );
+    return value
+      ? "I’ll always love you."
+      : "Even through the toughest times, I’ll still love you.";
   }
+  return "No matter what, my love for you is unconditional.";
 };
