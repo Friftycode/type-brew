c532db368a2e8f605b2ad3a1c4408d747b127c9d
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-16 15:07:59 +0200

    feat: Updated subtask 2. 3 and 4. Also removed task number on all variable names.

--- src/tasks/array-functions.ts
+++ src/tasks/array-functions.ts
@@ -1,30 +1,29 @@
 // Task 1: Combine two arrays.
-const taskOneCombineArrays = <T>(arr1: T[], arr2: T[]): T[] => {
+const combineArrays = <T>(arr1: T[], arr2: T[]): T[] => {
   return [...arr1, ...arr2];
 };
 
 // Task 2: Change out the last item in the array (without mutating original)
-const taskTwoReplaceLastItem = (arr: any[], newItem: any): any[] => {
-  return [...arr.slice(0, -1), newItem];
+const replaceLastItem = (arr: any[], newItem: any): any[] => {
+  arr.pop();
+  arr.push(newItem);
+  return arr;
 };
 
 // Task 3: Print the length of the array, add an item, print the length again.
-const taskThreePrintLengthBeforeAndAfter = (
-  arr: any[],
-  newItem: any,
-): any[] => {
+const printLengthBeforeAndAfter = (arr: any[], newItem: any): any[] => {
   console.log("Length before:", arr.length);
-  const newArray = [...arr, newItem];
-  console.log("Length after:", newArray.length);
-  return newArray;
+  arr.push(newItem);
+  console.log("Length after:", arr.length);
+  return arr;
 };
 
 // Task 4: Flatten arrays with generics and type safety
-const taskFourFlattenArray = (input: any[]): any[] => {
-  return input.flat(Infinity) as number[];
+const flattenArray = (input: any[]): any[] => {
+  return input.flat(Infinity);
 };
 
 // Task 5: Where did I put that again?
-const taskFiveFindItemPosition = <T>(arr: T[], item: T): number => {
+const findItemPosition = <T>(arr: T[], item: T): number => {
   return arr.indexOf(item);
 };
