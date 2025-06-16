78ca5319c4e90a0da003c089dbd25d4ecdd42a58
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-16 08:12:43 +0200

    feat: Updated code for subtask 2 and 4.

--- src/tasks/array-functions.ts
+++ src/tasks/array-functions.ts
@@ -3,11 +3,9 @@ const taskOneCombineArrays = <T>(arr1: T[], arr2: T[]): T[] => {
   return [...arr1, ...arr2];
 };
 
-// Task 2: Change out the last item in the array
-const taskTwoReplaceLastItem = <T>(arr: T[], newItem: T): T[] => {
-  arr.pop();
-  arr.push(newItem);
-  return arr;
+// Task 2: Change out the last item in the array (without mutating original)
+const taskTwoReplaceLastItem = (arr: any[], newItem: any): any[] => {
+  return [...arr.slice(0, -1), newItem];
 };
 
 // Task 3: Print the length of the array, add an item, print the length again.
@@ -17,9 +15,9 @@ const taskThreePrintLengthBeforeAndAfter = <T>(arr: T[], newItem: T): void => {
   console.log("Length after:", arr.length);
 };
 
-// Task 4: Flatten arrays
+// Task 4: Flatten arrays with generics and type safety
 const taskFourFlattenArray = (input: any[]): any[] => {
-  return input.flat(Infinity);
+  return input.flat(Infinity) as number[];
 };
 
 // Task 5: Where did I put that again?
