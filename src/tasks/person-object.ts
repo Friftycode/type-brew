234cad0c62b2e10df11ee6d7b4b5380807c2f3e0
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-25 10:07:26 +0200

    chore: Updated person-object.ts and people-are-moving.ts to use the new interface

--- src/tasks/person-object.ts
+++ src/tasks/person-object.ts
@@ -1,3 +1,9 @@
-export const createPerson = (name: string, age: number, city: string) => {
+import type { Person } from './people-interface';
+
+export const createPerson = (
+  name: string,
+  age: number,
+  city: string
+): Person => {
   return { name, age, city };
 };
