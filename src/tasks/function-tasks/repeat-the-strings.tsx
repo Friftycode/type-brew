90963d1a519df22004c5e08fc6421c0952d367fc
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-08-12 12:55:18 +0200

    fix: Update return type to (string | number)[] for consistent array typing

--- src/tasks/function-tasks/repeat-the-strings.tsx
+++ src/tasks/function-tasks/repeat-the-strings.tsx
@@ -1,7 +1,7 @@
 const repeatTheStrings = (
   strings: string[],
   times: number[]
-): string[] | number[] => {
+): (string | number)[] => {
   const minLength = Math.min(strings.length, times.length);
   for (let i = 0; i < minLength; i++) {
     for (let j = 0; j < times[i]; j++) {
